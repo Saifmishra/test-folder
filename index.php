56c933d3ec06952c00ba8a11617d2b1b5959675c
Saifmishra <you@example.com>
2021-07-30 03:38:45 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php
+
+for($i=0; $i<10; $i++){
+	for($k=9; $k>=$i; $k--){
+		echo '&nbsp ';
+	}
+	for($j=0; $j<=$i; $j++){
+		echo "*";
+	}
+	for($j=0; $j<$i; $j++){
+		echo "*";
+	}
+	echo "<br>";
+}
